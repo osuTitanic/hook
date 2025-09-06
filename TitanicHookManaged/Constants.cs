0b0fd6f044693638416925e4334e2e07c238e0c3
Lekuru <contact@lekuru.xyz>
2025-09-06 14:18:54 +0200

    Update release version to `1.0.0`

--- TitanicHookManaged/Constants.cs
+++ TitanicHookManaged/Constants.cs
@@ -15,5 +15,5 @@ public static class Constants
     public const string DefaultConfigName = "Titanic!.cfg";
     public const string LogFileName = "Titanic!.log";
     public const string UpdateServer = "http://hook.titanic.sh";
-    public const string PatchVersion = "0.2.0";
+    public const string PatchVersion = "1.0.0";
 }
